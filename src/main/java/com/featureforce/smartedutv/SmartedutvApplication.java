e46980efd2b9db3917194387e8a9410abb421c8b
nenowo <38612806+nenowoDev@users.noreply.github.com>
2026-06-24 22:17:22 +0800

    Init

---
+++ src/main/java/com/featureforce/smartedutv/SmartedutvApplication.java
@@ -0,0 +1,13 @@
+package com.featureforce.smartedutv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartedutvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartedutvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/featureforce/smartedutv/SmartedutvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.featureforce.smartedutv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartedutvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
